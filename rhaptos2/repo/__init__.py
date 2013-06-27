887c1401647a8ef3268ac1a05285983353d92241
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-06-27 14:40:41 +0100

    Alter set_up_logging and config
    so they can work together in turning off and on some of the logging options

--- rhaptos2/repo/__init__.py
+++ rhaptos2/repo/__init__.py
@@ -107,71 +107,90 @@ def make_app(config, as_standalone=False):
 
     # Try to set up logging. If not connected to a network this throws
     # "socket.gaierror: [Errno 8] nodename nor servname provided, or not known"
+    # Silences too much, tried to be specific.
     try:
         set_up_logging(app)
-    except:
+    except socket.gaierror, se:
         pass
+    except Exception, e:
+        raise e        
 
     # Set the application
     app = set_app(app)
 
-    # Initialize the views
-    # This import circular avoidinace trick is horrible
-    # I will review log and import process and want to put it all in a single
-    # setup in run.
-    from rhaptos2.repo import auth  # noqa
-    from rhaptos2.repo import views  # noqa
-
     return app
-
-
-def dolog(lvl, msg, caller=None, statsd=None):
-    """
-    reducing logging to minimum
-    """
-    lvls = {
-       "CRITICAL": 50,
-       "ERROR": 40,
-       "WARNING": 30,
-       "INFO": 20,
-       "DEBUG": 10,
-       "NOTSET": 0
-    }
-    try:
-        goodlvl = lvls[lvl]
-    except:
-        goodlvl = 20  # !!!
-    _lgr.log(goodlvl, msg)
-    
 
 
 def set_up_logging(app):
     """Set up the logging within the application.
 
+    We have three logging outputs - console, filesystem and syslog
+    syslog is only expected to exist for production purposes.
+    console by default logs only errors and is a convenience for developoers
+    filesystem is for digging deeper.
+
+    They are controlled via the following in .ini files::
+
+        loglevel = DEBUG
+        use_logging = Y
+        log_to_console = Y
+
+        log_to_filesystem = Y
+        local_log_dir = /opt/cnx/log/
+
+        log_to_syslog = Y
+        syslogfile = /dev/log
+
+    
     """
     config = app.config
 
     default_formatter = logging.Formatter(\
        "%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 
-    console_handler = StreamHandler()
-    console_handler.setFormatter(default_formatter)
-
-    error_handler = FileHandler("repo-error.log", "a")
-    error_handler.setLevel(logging.INFO)
-    error_handler.setFormatter(default_formatter)
-
-    flask_error_handler = FileHandler("flask-error.log", "a")
-    flask_error_handler.setLevel(logging.INFO)
-    flask_error_handler.setFormatter(default_formatter)
-
-    
+    ### root logger - used everywhere *except*
+    ### log messages sent during configuration collection.
     root = logging.getLogger(__name__)
-    root.addHandler(console_handler)
-    root.addHandler(error_handler)
-    root.setLevel(logging.INFO)
-
-    root.info("logger set up on %s" % __name__)
+    root.setLevel(config['globals']['loglevel'])
     
-    app.logger.addHandler(flask_error_handler)
-    app.logger.info("Flask-logger will output to %s" % "flask-error.log")
+    ### console output - turn off in config, simplified output.
+    if config['globals']['log_to_console'] == 'Y':
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.ERROR)
+        console_handler.setFormatter("%(levelname)s : %(message)s")    
+        root.addHandler(console_handler)
+    
+    ## write to syslog - defaults to INFO
+    if config['globals']['log_to_syslog'] == 'Y':
+        syslog_handler = logging.handlers.SysLogHandler(address=config['globals']['syslogfile'])
+        syslog_handler.setLevel(logging.INFO)
+        syslog_handler.setFormatter(default_formatter)
+        root.addHandler(syslog_handler)
+    
+
+
+    ### local file loggers for development
+    ### eventlog records everything, errorlog just for errors
+    if config['globals']['log_to_filesystem'] == 'Y':
+
+        eventlogpath = os.path.join(config['globals']['local_log_dir'],
+                                  "repo-error.log")
+
+        errlogpath = os.path.join(config['globals']['local_log_dir'],
+                                  "repo-error.log")
+
+        error_handler = logging.FileHandler(errlogpath, "a")
+        error_handler.setLevel(logging.ERROR)
+        error_handler.setFormatter(default_formatter)
+
+        event_handler = logging.FileHandler(eventlogpath, "a")
+        event_handler.setLevel(logging.INFO)
+        event_handler.setFormatter(default_formatter)
+
+        root.addHandler(error_handler)
+        root.addHandler(event_handler)
+
+      
+    root.info("logger set up on %s as %s" % (__name__, str(root)))
+    
+    
