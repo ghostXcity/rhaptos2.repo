064d84d9358f7ca5717b1386739f11903596c8b7
Paul Brian (lifeisstillgood) <paul@mikadosoftware.com>
2013-07-08 12:19:13 +0100

    Clean up test dir, one shell script for API tests

--- rhaptos2/repo/tests/run_populateonce.sh
+++
@@ -1,6 +0,0 @@
-python cleardb.py
-nosetests --tc-file=../../../testing.ini test_wsgi.py:test_post_module
-nosetests --tc-file=../../../testing.ini test_wsgi.py:test_post_collection
-nosetests --tc-file=../../../testing.ini test_wsgi.py:test_post_folder
-
-
